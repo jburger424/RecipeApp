d75a2c0e21f3bbbec85f44af824056a5311e8bdb
Isaak <ihill@ithaca.edu>
2018-03-27 00:02:00 -0400

    Implemented basic functionality

--- src/main/java/CommandLine.java
+++ src/main/java/CommandLine.java
@@ -1,8 +1,57 @@
+import java.util.Scanner;
+
 /**
  * Isaak Hill RecipeApp 3/25/18.
  */
 public class CommandLine {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    private Scanner userScan;
+
+    public CommandLine() {
+        this.userScan=new Scanner(System.in);
+    }
+
+    public void run(){
+        while(true) {
+            System.out.println(
+                    "RecipeApp \n" +
+                            "Available commands are: \n" +
+                            "Help\n" +
+                            "List\n" +
+                            "View [ID]");
+            System.out.print(">_");
+            String cmd = userScan.next();
+            switch (cmd) {
+                case "List":
+                case "list":
+                case "l":
+                    System.out.println("List recipes is not implemented yet...\n");
+                    break;
+                case "help":
+                case "Help":
+                case "h":
+                    System.out.println("Help: \n" +
+                            "The help command prints a menu of other commands and what they do \n");
+                    break;
+                case "quit":
+                case "Quit":
+                case "q":
+                    System.out.println("Thank you for using RecipeApp!\n");
+                    return;
+                default:
+                    System.out.println("Command not recognized, please try again...\n");
+                    break;
+            }
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ie) {
+                System.out.println("Really not sure how this happened...");
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        CommandLine cl = new CommandLine();
+        cl.run();
     }
 }
