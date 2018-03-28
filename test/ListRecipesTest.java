00dc0d1a595f1e6eccd4e1b65b3a795dacc6d1d2
Austin Barrett <austinbarrett52@gmail.com>
2018-03-28 09:35:52 -0400

    Created Test Class for ListRecipes.java

---
+++ test/ListRecipesTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListRecipesTest {
+
+    @Test
+    void printAllRecipeInfo() {
+    }
+
+    @Test
+    void main() {
+    }
+
+}
