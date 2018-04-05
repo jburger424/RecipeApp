8e70ce0ca0b9dd9704e119fa3c48028c4673d106
Isaak <ihill@ithaca.edu>
2018-04-05 09:57:21 -0400

    Help command abstracted

--- src/main/java/ihill/CommandLine/CommandLine.java
+++ src/main/java/ihill/CommandLine/CommandLine.java
@@ -33,11 +33,7 @@ public class CommandLine {
                 case "help":
                 case "Help":
                 case "h":
-                    System.out.println("Help: \n" +
-                            "The help command prints a menu of other commands and what they do \n" +
-                            "List: Lists available rooms\n" +
-                            "View [ID]: Views the details of the room numbered [ID]\n" +
-                            "Quit: Quits the program\n\n");
+                    help();
                     break;
                 case "view":
                 case "View":
@@ -74,6 +70,13 @@ public class CommandLine {
     private void list(){
         System.out.println("Listing recipes...\n");
         System.out.println("List recipes not implemented yet\n");
+    }
+    private void help(){
+        System.out.println("Help: \n" +
+                "The help command prints a menu of other commands and what they do \n" +
+                "List: Lists available rooms\n" +
+                "View [ID]: Views the details of the room numbered [ID]\n" +
+                "Quit: Quits the program\n\n");
     }
     public static void main(String[] args) throws Exception {
         CommandLine cl = new CommandLine();
