bad480352eb0d13bb1f4217bc978cd12c9579b56
Austin Barrett <austinbarrett52@gmail.com>
2018-04-30 18:36:08 -0400

    Worked on database query within WindowDisplay

--- src/main/java/edu/ithaca/recipeApp/WindowDisplay.java
+++ src/main/java/edu/ithaca/recipeApp/WindowDisplay.java
@@ -6,10 +6,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.sql.*;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class WindowDisplay {
+    int id;
+    int calories;
+    int servings;
+    String title;
+    String[] ingredients;
+    String[] steps;
+
+    public WindowDisplay(int id){
+        this.id = id;
+    }
 
     public static void getImage(String imageUrl) throws Exception {
         imageUrl = "http://www.avajava.com/images/avajavalogo.jpg";
@@ -49,6 +61,75 @@ public class WindowDisplay {
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
+
+    }
+
+    public void getDatabaseInfo(int ID){
+//        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        // load the sqlite-JDBC driver using the current class loader
+        try{
+            Class.forName("org.sqlite.JDBC");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        Connection connection = null;
+        try {
+            // create a database connection
+            connection = DriverManager.getConnection("jdbc:sqlite:src/test/resources/db/recipes.db");
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            ResultSet rs = statement.executeQuery("select * from RECIPE_TO_INGREDIENT WHERE RECIPE_ID="+ID);
+            while(rs.next()) {
+                int ingredID = rs.getInt("INGREDIENT_ID");
+                String ingredQuant = rs.getString("QUANTITY");
+                Ingredient tempIngred = new Ingredient();
+                tempIngred.setID(ingredID);
+                tempIngred.setQuantity(ingredQuant);
+                ingredients.add(tempIngred);
+            }
+            for (Ingredient i:ingredients){
+                rs = statement.executeQuery("select * from INGREDIENTS WHERE ID="+i.getID());
+                i.setName(rs.getString("NAME"));
+            }
+            rs = statement.executeQuery("select * from RECIPES WHERE ID="+ID);
+            while(rs.next())
+            {
+
+                title = rs.getString("title");
+//                System.out.println("Ingredients:");
+//                for(Ingredient i: ingredients){
+//                    System.out.println("\t"+i.toString());
+//                }
+                ingredients = rs.getString("Ingredient").split("\\r?\\n|\\r");
+                servings = rs.getInt("SERVINGS");
+                calories = rs.getInt("CALS_PER_SERVING");
+                steps = rs.getString("steps").split("\\r?\\n|\\r");
+//                for(int i = 0; i < steps.length; i++){
+//                    steps[i] = ("\t"+);
+//                }
+            }
+        }
+        catch(SQLException e)
+        {
+            // if the error message is "out of memory",
+            // it probably means no database file is found
+            System.err.println(e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                if(connection != null)
+                    connection.close();
+            }
+            catch(SQLException e)
+            {
+                // connection close failed.
+                System.err.println(e);
+            }
+        }
 
 
     }
